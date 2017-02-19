8b0c7f398917e2dbab848e1e63d28c32d6d19230
Nguyễn Đình Đạt <datnd17101995@gmail.com>
2017-02-20 03:43:54 +0700

    migration and repository

---
+++ Shop.Data/Repositories/MenuGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+
+namespace Shop.Data.Repositories
+{
+    public interface IMenuGroupRepository : IRepository<MenuGroups>
+    {
+    }
+
+    public class MenuGroupRepository : RepositoryBase<MenuGroups>, IMenuGroupRepository
+    {
+        public MenuGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
